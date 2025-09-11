8609bc0ee68d1904f544aa3358f86b98546d736b
Joserraul <joserraul07@gmail.com>
2025-09-11 13:55:18 -0400

    funcion del carrito

--- Entrega/src/router/router_Cart.js
+++ Entrega/src/router/router_Cart.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.post("/", cartManager.createCart.bind(cartManager));
 router.get("/:cid", cartManager.getCartById.bind(cartManager));
-router.post("/:cid/product/:pid", cartManager.addProductToCart.bind(cartManager));
+router.post("/:cid/products/:pid", cartManager.addProductToCart.bind(cartManager));
 router.delete("/:cid/products/:pid", cartManager.deleteProductFromCart.bind(cartManager));
 router.put("/:cid", cartManager.updateCart.bind(cartManager));
 router.put("/:cid/products/:pid", cartManager.updateProductQuantity.bind(cartManager));
